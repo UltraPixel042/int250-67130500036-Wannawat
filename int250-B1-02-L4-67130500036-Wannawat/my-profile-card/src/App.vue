bf6b0269f31bdb6213282e3994dfd9949b0d7d6b
Wannawat S. <UltraPixel042@users.noreply.github.com>
2026-02-01 22:12:50 +0700

    B1-02-L4/All - Submitted my-profile-card

---
+++ int250-B1-02-L4-67130500036-Wannawat/my-profile-card/src/App.vue
@@ -0,0 +1,46 @@
+<script setup>
+import ProfileCard from "./components/ProfileCard.vue";
+import profilePic from "./assets/profile.png";
+import bgPic from "./assets/background.jpg";
+const myProfile = {
+  background: bgPic,
+  profileimg: profilePic,
+  name: "Wannawat Siriarkanay",
+  subheading: "Student, 20, BKK",
+  followers: "475",
+  following: "1114",
+  projects: "10+",
+  facebook: "https://www.facebook.com/wannawat.siriarkanay",
+  instagram: "https://www.instagram.com/4thvowelineng/",
+  linkedin: "https://www.linkedin.com/public-profile/settings?trk=d_flagship3_profile_self_view_public_profile"
+};
+</script>
+
+<template>
+  <div
+    class="flex items-center justify-center gap-8 min-h-screen bg-linear-to-br from-green-500 to-sky-400"
+  >
+    <ProfileCard 
+      background="./src/assets/background.jpg"
+      :profileimg="myProfile.profileimg"
+      :background="myProfile.background"
+      :name="myProfile.name"
+      :subheading="myProfile.subheading"
+      :followers="myProfile.followers"
+      :following="myProfile.following"
+      :projects="myProfile.projects"
+    />
+    <ProfileCard 
+      background="./src/assets/background.jpg"
+      :profileimg="myProfile.profileimg"
+      :background="myProfile.background"
+      :name="myProfile.name"
+      :subheading="myProfile.subheading"
+      :facebook="myProfile.facebook"
+      :instagram="myProfile.instagram"
+      :linkedin="myProfile.linkedin"
+    />
+  </div>
+</template>
+
+<style scoped></style>
