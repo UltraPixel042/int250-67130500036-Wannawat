19e67e4c380a4659b2fb8b6f87e668f33bc1bd9c
Wannawat Siriarkanay <177916274+UltraPixel042@users.noreply.github.com>
2026-03-21 21:49:37 +0700

    Modified App.vue

--- int250-B2-02-L6-67130500036-Wannawat/loopstudios-website/src/App.vue
+++ int250-B2-02-L6-67130500036-Wannawat/loopstudios-website/src/App.vue
@@ -1,22 +1,3 @@
-<!-- <script setup>
-    import NavBar from './components/NavBar.vue';
-    import Feature from './components/Feature.vue';
-    import Creation from './components/Creation.vue';
-    import Footer from './components/Footer.vue';
-</script>
-
-<template>
-    <NavBar />
-    <Feature />
-    <Creation />
-    <Footer />
-</template>
-
-<style scoped>
-
-</style> -->
-
-
 <script setup>
 import NavBar from './components/NavBar.vue';
 import Feature from './components/Feature.vue';
