4832829d6f7efaae126abb4ac8c18e4a2a07d0e0
Wannawat Siriarkanay <177916274+UltraPixel042@users.noreply.github.com>
2026-05-11 16:30:16 +0700

    Modified code

--- int250-B3-02-L7-67130500036-Wannawat/fylo-website/src/components/FAQItem.vue
+++ int250-B3-02-L7-67130500036-Wannawat/fylo-website/src/components/FAQItem.vue
@@ -11,7 +11,13 @@ const isOpen = ref(false);
 
 <template>
     <div class="border-b border-slate-500">
-        <div @click="isOpen = !isOpen" class="flex justify-between items-center cursor-pointer py-2">
+        <div
+            @click="isOpen = !isOpen"
+            class="flex justify-between items-center cursor-pointer py-2"
+            role="button"
+            :aria-expanded="isOpen"
+            aria-controls="faq-answer"
+        >
             <span class="text-md">{{ question }}</span>
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                 class="bi bi-chevron-down transition-transform duration-300" :class="isOpen ? 'rotate-180' : 'rotate-0'" viewBox="0 0 16 16">
@@ -19,7 +25,7 @@ const isOpen = ref(false);
                     d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708" />
             </svg>
         </div>
-        <p v-show="isOpen" class="text-start text-sm text-slate-500 px-4 pb-2">{{ answer }}</p>
+        <p id="faq-answer" v-show="isOpen" class="text-start text-sm text-slate-500 px-4 pb-2">{{ answer }}</p>
     </div>
 </template>
 
